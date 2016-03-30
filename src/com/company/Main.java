dee4cb6444e8accc3c7a904c5dc707a05f1cb78d
Moon <moonseopk@gmail.com>
2016-03-29 23:37:45 -0500

    finished remove

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
         MyHash md = new MyHash();
-        md.write("a", "ab");
-        md.write("b", "cd");
-        for (String s : md.getKeys()) {
-            System.out.println(s + ",");
-        }
+        md.write("number1", "one");
+        md.write("number2", "two");
+        md.write("number4", "four");
+        md.write("number5", "five");
+        md.write("number3", "three");
+        md.write("number7", "seven");
+
+        System.out.println(md.getKeys().toString());
+        //System.out.println(md.read("number3"));
+        md.delete("number4");
+        md.delete("number2");
+
+        System.out.println(md.getKeys().toString());
+        md.write("number10", "ten");
+        System.out.println(md.getKeys().toString());
+
 	// write your code here
     }
 
--- src/com/company/MyHash.java
+++ src/com/company/MyHash.java
@@ -17,12 +17,14 @@ public class MyHash implements MyDataType {
     public MyHash() {
         hm = new HashMap<String, String>();
         hm2 = new HashMap<String, String>();
+        hm3 = new HashMap<String, String>();
         count = 0;
     }
 
     public void write(String key, String value) {
         hm.put(key, value);
         hm2.put(count, key);
+        hm3.put(key, count);
         count++;
     }
 
@@ -33,17 +35,28 @@ public class MyHash implements MyDataType {
 
     public List<String> getKeys() {
         List<String> keylist = new ArrayList<String>(); // ERROR
-        for(int i = 0; i<hm2.size(); i++) {
-            String k = (String) hm2.get(i);
-            keylist.add(k);
+        //System.out.println(hm2.size());
+        for(int i = 0; i < hm3.size(); i++) {
+            //System.out.println(hm2.get(i));
+            if(hm2.get(i) != null) {
+                String k = (String) hm2.get(i);
+                keylist.add(k);
+            }
         }
         return keylist;
     }
 
     public void delete(String key) {
+        //System.out.println(hm);
         hm.remove(key);
-        hm2.get();
-        hm2.remove();
+        //System.out.println(hm);
+        int s = (int) hm3.get(key);
+        //System.out.println("The deleted : "+ s);
+        //System.out.println(hm2);
+        hm2.remove(s);
+        //System.out.println(hm2);
+        //hm2.get();
+        //hm2.remove();
     }
 
 }
